c2f98ff705747db98ba2afa47999d9672c996f70
Biplav-05 <biplav413@gmail.com>
2024-10-22 11:17:34 +0545

    fix: new alpha

--- MathLibrary/MathOperations.cs
+++ MathLibrary/MathOperations.cs
@@ -6,7 +6,7 @@ namespace MathLibrary
     {
         public int Add(int a, int b)
         {
-            return a + b;
+            return a + b*2;
         }
 
         public int Subtract(int a, int b)
